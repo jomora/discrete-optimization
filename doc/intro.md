4e7885e6b3c44439138b7d433e76ae4ef1417b90
Jonas Molina Ramirez <jonas.molina@t-online.de>
2020-09-23 00:11:53 +0200

    Initial commit
    Contains the basic leiningen project structure
    created with:
    
    lein new discrete-optimizaton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to discrete-optimization
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/discrete_optimization/core_test.clj
@@ -0,0 +1,7 @@
+(ns discrete-optimization.core-test
+  (:require [clojure.test :refer :all]
+            [discrete-optimization.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
